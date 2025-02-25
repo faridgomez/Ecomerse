0a7ff741a209672e49b7f242cd9efd7d14d45139
faridgomez <ddbbequipo@gmail.com>
2025-02-24 20:08:36 -0500

    Se crean y paginas de mostrar y crear pdoductos , se estable direccion en controlador

--- src/main/java/com/First/Ecomerse/Controller/ProductosControllador.java
+++ src/main/java/com/First/Ecomerse/Controller/ProductosControllador.java
@@ -3,13 +3,21 @@ package com.First.Ecomerse.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/Productos")
 public class ProductosControllador {
     @GetMapping("")
     public String show() {
-        return "Productos/show";
+        return "Productos/Mostrar";
     }
+
+    @GetMapping("/crearProductos")
+    public String create() {
+        return "Productos/crear";
+    }
+    
 
 }
