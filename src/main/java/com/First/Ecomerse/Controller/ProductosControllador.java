38d60a6dabb610367bce309f9c2532f78e85a735
faridgomez <ddbbequipo@gmail.com>
2025-02-26 14:48:55 -0500

    Se realiza funcionalidad Guardar(save) en productos,se esta quemando el usuario por el momento

--- src/main/java/com/First/Ecomerse/Controller/ProductosControllador.java
+++ src/main/java/com/First/Ecomerse/Controller/ProductosControllador.java
@@ -1,12 +1,15 @@
 package com.First.Ecomerse.Controller;
 
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import com.First.Ecomerse.Model.Productos;
+import com.First.Ecomerse.Model.Usuario;
+import com.First.Ecomerse.Service.ProductoService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,6 +18,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ProductosControllador {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ProductosControllador.class);
+    
+    @Autowired  //Inyección de dependencias
+    private ProductoService productoService;
 
     @GetMapping("")
     public String show() {
@@ -29,6 +35,11 @@ public class ProductosControllador {
     @PostMapping("/save")
     public String save(Productos productos) {
         LOGGER.info("este es el producto {}", productos);
+        
+        Usuario U = new Usuario(1, null, null, null, null, null, null, null, null);
+        productos.setUsuarios(U);
+        
+        productoService.save(productos);
         return "redirect:/Productos";
     }
 
